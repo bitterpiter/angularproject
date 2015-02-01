d815648af533018c853de09c8cacc8c66aabc694
bitterpiter <bitter.piter@gmail.com>
2015-02-01 09:38:19 +0000

    add first templates

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,27 @@
 var myApp = angular.module("adminPanel", ["ui.router", "firebase"]);
+
 myApp.constant('FIREBASE_URI', "https://website-techs.firebaseio.com/");
 myApp.config(function($stateProvider, $urlRouterProvider) {
-
+    $stateProvider
+        .state('dashboard', {
+            url: '/dashboard',
+            templateUrl: 'templates/dashboard.html'
+        })
+        .state('categories', {
+            url: '/categories',
+            templateUrl: 'templates/categories.html'
+        })
+        .state('add-item', {
+            url: '/add-item',
+            templateUrl: 'templates/add-item.html',
+            controller: 'ListCtrl'
+        })
+        .state('list.item', {
+            url: '/:item',
+            templateUrl: 'templates/list.item.html',
+            controller: function($scope, $stateParams) {
+                $scope.item = $stateParams.item;
+            }
+        });
+    $urlRouterProvider.otherwise('/dashboard');
 });
--- assets/js/factories/menu_factory.js
+++ assets/js/factories/menu_factory.js
@@ -1,11 +1,11 @@
 myApp.factory("MenuPositions", function() {
     var menuPositions = [
-        {href: "index.html", icon: "fa-dashboard", title: "Dashboard"},  
-        {href: "index.html", icon: "fa-desktop", title: "Position #1"},  
+        {href: "dashboard", icon: "fa-dashboard", title: "Dashboard"},  
+        {href: "categories", icon: "fa-desktop", title: "Position #1"},  
         {href: "index.html", icon: "fa-qrcode", title: "Position #2"},  
         {href: "index.html", icon: "fa-bar-chart-o", title: "Position #3"},  
         {href: "index.html", icon: "fa-table", title: "Position #4"},  
         {href: "index.html", icon: "fa-edit", title: "Position #5"},  
     ];
     return menuPositions;
-});
+});s
