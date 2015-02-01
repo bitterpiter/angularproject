717bcb9d62614591d99dae200b22e0a25dbead84
bitterpiter <bitter.piter@gmail.com>
2015-02-01 09:12:16 +0000

    other changes

--- assets/js/clock.js
+++ assets/js/clock.js
@@ -1,14 +1,41 @@
 function startTime() {
+    var month = new Array();
+    month[0] = "sty";
+    month[1] = "lut";
+    month[2] = "mar";
+    month[3] = "kwi";
+    month[4] = "maj";
+    month[5] = "cze";
+    month[6] = "lip";
+    month[7] = "sie";
+    month[8] = "wrz";
+    month[9] = "paź";
+    month[10] = "lis";
+    month[11] = "gru";
+    var weekdayArray = new Array(7);
+    weekdayArray[0]=  "nie";
+    weekdayArray[1] = "pon";
+    weekdayArray[2] = "wto";
+    weekdayArray[3] = "śro";
+    weekdayArray[4] = "czw";
+    weekdayArray[5] = "pią";
+    weekdayArray[6] = "sob";
+    
+    
+    
+    
+    
     var today=new Date();
+    var weekday = weekdayArray[today.getDay()];
     var h=today.getHours();
     var m=today.getMinutes();
     var s=today.getSeconds();
-    var day=today.getDay();
-    var month=today.getMonth();
+    var day= today.getUTCDate();
+    var n = month[today.getMonth()];
     var year=today.getFullYear();
     m = checkTime(m);
     s = checkTime(s);
-    document.getElementById('clock').innerHTML = day + "." + month + "." + year + " - " + h + ":" + m + ":" + s;
+    document.getElementById('clock').innerHTML = weekday + " " + day + " " + n + " " + h + ":" + m + ":" + s;
     var t = setTimeout(function(){startTime()},500);
 }
 
